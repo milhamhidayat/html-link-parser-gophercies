4a2eba84962a918c5ea756b17f9316292b51d797
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2018-10-03 22:07:57 +0700

    DFSing the HTML

--- link/parse.go
+++ link/parse.go
@@ -1,7 +1,10 @@
 package link
 
 import (
+	"fmt"
 	"io"
+
+	"golang.org/x/net/html"
 )
 
 // Link represents a link in an HTML
@@ -14,5 +17,21 @@ type Link struct {
 // Parse will take in an HTML document and will return a
 // slice of links parsed from it
 func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	dfs(doc, "")
 	return nil, nil
+}
+
+func dfs(n *html.Node, padding string) {
+	msg := n.Data
+	if n.Type == html.ElementNode {
+		msg = "<" + msg + ">"
+	}
+	fmt.Println(padding, msg)
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		dfs(c, padding+"  ")
+	}
 }
